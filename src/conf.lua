b1b3fcee563be7e24365918f37b08c900cdfb041
Antoine Gersant <antoine.gersant@lesforges.org>
2016-02-07 13:11:42 -0800

    Added hello world

---
+++ src/conf.lua
@@ -0,0 +1,6 @@
+love.conf = function( options )
+	options.console = true;
+	options.window.width = 1280;
+	options.window.height = 720;
+	options.window.resizable = true;
+end
---
+++ src/main.lua
@@ -0,0 +1,3 @@
+love.draw = function()
+	love.graphics.print( "Oink oink!", 40, 20 );
+end 
