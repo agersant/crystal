5e4616c82089e239e971fcb38e9ec608f083dc65
Antoine Gersant <antoine.gersant@lesforges.org>
2016-02-08 21:05:49 -0800

    Implemented basic OOP

---
+++ src/utils/oop.lua
@@ -0,0 +1,36 @@
+local OOP = {};
+
+local makeClass = function( name, baseClass )
+	local classMeta = {};
+	classMeta.__index = baseClass;
+	classMeta.__tostring = function( class )
+		return "class definition of '" .. class._name .. "'";
+	end
+	return setmetatable( {}, classMeta );
+end
+
+OOP.class = function( name, baseClass )
+	
+	local class = makeClass( name, baseClass );
+	
+	local objMetaTable = {};
+	objMetaTable.__index = class;
+	objMetaTable.__tostring = function( obj )
+		return "instance of class '" .. obj._class._name .. "'";
+	end
+	
+	class.super = baseClass;
+	class._class = class;
+	class._name = name;
+	class._objMetaTable = objMetaTable;
+	
+	class.new = function( class, ... )
+		local obj = setmetatable( {}, class._objMetaTable );
+		obj:init( ... );
+		return obj;
+	end
+	
+	return class;
+end
+
+return OOP;
