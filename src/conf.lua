279be91d0748530e7fb694d31e5850bcb07dad34
Antoine Gersant <antoine.gersant@lesforges.org>
2016-02-09 00:55:03 -0800

    Turned off undesirable modules

--- src/conf.lua
+++ src/conf.lua
@@ -9,4 +9,22 @@ love.conf = function( options )
 	options.window.width = 1280;
 	options.window.height = 720;
 	options.window.resizable = true;
+	
+	options.modules.audio = true;
+	options.modules.event = true;
+	options.modules.graphics = true;
+	options.modules.image = true;
+	options.modules.joystick = true;
+	options.modules.keyboard = true;
+	options.modules.math = true;
+	options.modules.mouse = true;
+	options.modules.physics = true;
+	options.modules.sound = true;
+	options.modules.timer = true;
+	options.modules.window = true;
+
+	options.modules.system = false;
+	options.modules.touch = false;
+	options.modules.video = false;
+	options.modules.thread = false;
 end
