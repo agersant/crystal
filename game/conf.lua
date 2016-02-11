d1b1b7f73e04a13cf58be038d8535ab51ad8fde9
Antoine Gersant <antoine.gersant@lesforges.org>
2016-02-11 00:07:05 -0800

    Updated window title

--- game/conf.lua
+++ game/conf.lua
@@ -7,6 +7,7 @@ gConf.features.cli = not release;
 
 love.conf = function( options )
 	options.console = true;
+	options.window.title = "Crystal";
 	options.window.width = 1280;
 	options.window.height = 720;
 	options.window.resizable = true;
