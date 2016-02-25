807017801e86724dd364cfb41add2e6749b18967
Antoine Gersant <antoine.gersant@lesforges.org>
2016-02-24 23:30:28 -0800

    Output cleanup

--- game/src/utils/OOP.lua
+++ game/src/utils/OOP.lua
@@ -2,7 +2,7 @@ local makeClass = function( name, baseClass )
 	local classMeta = {};
 	classMeta.__index = baseClass;
 	classMeta.__tostring = function( class )
-		return "Class definition of '" .. class._name .. "'";
+		return "Class definition of: " .. class._name;
 	end
 	return setmetatable( {}, classMeta );
 end
@@ -14,7 +14,7 @@ Class = function( name, baseClass )
 	local objMetaTable = {};
 	objMetaTable.__index = class;
 	objMetaTable.__tostring = function( obj )
-		return "Instance of class '" .. obj._class._name .. "'";
+		return "Instance of class: " .. obj._class._name;
 	end
 	
 	class.super = baseClass;
