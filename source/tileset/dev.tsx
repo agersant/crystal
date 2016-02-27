61ee70e54483d20a26023fde1f301169d195233b
Antoine Gersant <antoine.gersant@lesforges.org>
2016-02-27 00:22:17 -0800

    Added diagonal collision shapes to test map

--- source/tileset/dev.tsx
+++ source/tileset/dev.tsx
@@ -6,4 +6,32 @@
    <object id="0" x="0" y="0" width="16" height="16"/>
   </objectgroup>
  </tile>
+ <tile id="3">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="16">
+    <polygon points="0,0 16,-16 16,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="4">
+  <objectgroup draworder="index">
+   <object id="0" x="-0.0222222" y="0.0444444">
+    <polygon points="0,0 16.0222,15.9556 0.0222222,15.9556"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="11">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0">
+    <polygon points="0,0 16,0 16,16"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="12">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0">
+    <polygon points="0,0 16,0 0,16"/>
+   </object>
+  </objectgroup>
+ </tile>
 </tileset>
