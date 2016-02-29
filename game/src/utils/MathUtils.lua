ff2ebad80f655be00ed4b817da2103203e124753
Antoine Gersant <antoine.gersant@lesforges.org>
2016-02-28 20:47:45 -0800

    Added utility functions to snap angles to directions

--- game/src/utils/MathUtils.lua
+++ game/src/utils/MathUtils.lua
@@ -10,6 +10,36 @@ MathUtils.round = function( n )
 	return math.floor( n + 0.5 );
 end
 
+MathUtils.snapAngle = function( angle, numDirections )
+	angle = angle % 360;
+	assert( numDirections > 0 );
+	local angleStep = 360 / numDirections;
+	local stepCount = math.floor( .5 + angle / 360 * numDirections ) % numDirections;
+	return angleStep * stepCount;
+end
+
+MathUtils.angleToDir8 = function( angle )
+	local snappedAngle = MathUtils.snapAngle( angle, 8 );
+	if snappedAngle == 0 then
+		return 1, 0;
+	elseif snappedAngle == 45 then
+		return 1, 1;
+	elseif snappedAngle == 90 then
+		return 0, 1;
+	elseif snappedAngle == 135 then
+		return -1, 1;
+	elseif snappedAngle == 180 then
+		return -1, 0;
+	elseif snappedAngle == 225 then
+		return -1, -1;
+	elseif snappedAngle == 270 then
+		return 0, -1;
+	elseif snappedAngle == 315 then
+		return 1, -1;
+	end
+	error( "Unexpected angle: " .. tostring( snappedAngle ) );
+end
+
 
 
 return MathUtils;
--- game/src/utils/TestMathUtils.lua
+++ game/src/utils/TestMathUtils.lua
@@ -20,4 +20,25 @@ tests[#tests].body = function()
 	assert( y == 1 );
 end
 
+tests[#tests + 1] = { name = "Snap angle" };
+tests[#tests].body = function()
+	assert( 0 == MathUtils.snapAngle( 0, 4 ) );
+	assert( 90 == MathUtils.snapAngle( 90, 4 ) );
+	assert( 180 == MathUtils.snapAngle( 180, 4 ) );
+	assert( 270 == MathUtils.snapAngle( 270, 4 ) );
+	assert( 0 == MathUtils.snapAngle( 360, 4 ) );
+	assert( 0 == MathUtils.snapAngle( 20, 4 ) );
+	assert( 90 == MathUtils.snapAngle( 80, 4 ) );
+	assert( 0 == MathUtils.snapAngle( 330, 4 ) );
+end
+
+tests[#tests + 1] = { name = "Angle to dir 8" };
+tests[#tests].body = function()
+	assert( 1, 0 == MathUtils.angleToDir8( 0 ) );
+	assert( 1, 0 == MathUtils.angleToDir8( 20 ) );
+	assert( 1, 1 == MathUtils.angleToDir8( 30 ) );
+	assert( 0, 1 == MathUtils.angleToDir8( 80 ) );
+	assert( 1, 0 == MathUtils.angleToDir8( 350 ) );
+end
+
 return tests;
