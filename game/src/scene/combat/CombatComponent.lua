7187f0ba5150b1531682f2a32704c42e841c346d
Antoine Gersant <antoine.gersant@lesforges.org>
2016-02-29 22:53:26 -0800

    Implemented variable attack damage, defense and crit

--- game/src/scene/combat/CombatComponent.lua
+++ game/src/scene/combat/CombatComponent.lua
@@ -6,16 +6,38 @@ local CombatComponent = Class( "CombatComponent" );
 
 
 
+-- IMPLEMENTATION
+
+local mitigateDamage = function( self, damage )
+	local rawAmount = damage:getAmount();
+	local defense = self._defense:getValue();
+	local mitigationFactor = defense / ( defense + 100 );
+	local mitigatedAmount = rawAmount * ( 1 - mitigationFactor );
+	return mitigatedAmount;
+end
+
+
+
 -- PUBLIC API
 
 CombatComponent.init = function( self, entity )
 	assert( entity );
 	self._entity = entity;
-	self._health = CombatStat:new( 40, 0, nil );
+	self._health = CombatStat:new( 500, 0, nil );
+	self._defense = CombatStat:new( 10, 1, nil );
+	self._strength = CombatStat:new( 10, 1, nil );
+	self._attackRating = CombatStat:new( 1.5, 0, nil );
+	self._critRate = CombatStat:new( .02, 0, 1 );
+	self._critRating = CombatStat:new( 2, 1, nil );
 end
 
 CombatComponent.inflictDamageTo = function( self, target )
-	local damage = Damage:new( 10, self._entity ); -- TODO proper amount
+	local effectiveStrength = self._strength:getValue() * self._attackRating:getValue();
+	local isCrit = math.random() < self._critRate:getValue();
+	if isCrit then
+		effectiveStrength = effectiveStrength * self._critRating:getValue();
+	end
+	local damage = Damage:new( effectiveStrength, self._entity );
 	target:receiveDamage( damage );
 end
 
@@ -24,8 +46,8 @@ CombatComponent.receiveDamage = function( self, damage )
 		return;
 	end
 	self._entity:signal( "takeHit", damage );
-	local amount = damage:getAmount();
-	self._health:substract( amount );
+	local effectiveDamage = mitigateDamage( self, damage );
+	self._health:substract( effectiveDamage );
 	if self:isDead() then
 		self._entity:signal( "death" );
 	end
