258b9c252ee8596951a5671f99b30b517521013e
Antoine Gersant <antoine.gersant@lesforges.org>
2016-03-06 22:15:38 -0800

    Adjusted imprecise collision shapes

--- source/tileset/dev.tsx
+++ source/tileset/dev.tsx
@@ -22,15 +22,15 @@
  </tile>
  <tile id="5">
   <objectgroup draworder="index">
-   <object id="0" x="15.75" y="8">
-    <polygon points="0,0 -0.125,7.875 -7.75,8"/>
+   <object id="0" x="16" y="8">
+    <polygon points="0,0 0,8 -8,8"/>
    </object>
   </objectgroup>
  </tile>
  <tile id="6">
   <objectgroup draworder="index">
-   <object id="0" x="-0.25" y="7.75">
-    <polygon points="0,0 8.25,8.125 0.25,8.125"/>
+   <object id="0" x="0" y="8">
+    <polygon points="0,0 8,8 0,8"/>
    </object>
   </objectgroup>
  </tile>
@@ -50,8 +50,8 @@
  </tile>
  <tile id="13">
   <objectgroup draworder="index">
-   <object id="0" x="7.875" y="0.125">
-    <polygon points="0,0 7.875,8 8.25,-0.125"/>
+   <object id="0" x="8" y="0">
+    <polygon points="0,0 8,0 8,8"/>
    </object>
   </objectgroup>
  </tile>
@@ -64,47 +64,47 @@
  </tile>
  <tile id="16">
   <objectgroup draworder="index">
-   <object id="0" x="8.125" y="0.125" width="7.75" height="16"/>
+   <object id="0" x="8" y="0" width="8" height="16"/>
   </objectgroup>
  </tile>
  <tile id="17">
   <objectgroup draworder="index">
-   <object id="0" x="-0.125" y="0.125" width="7.875" height="16"/>
+   <object id="0" x="0" y="0" width="8" height="16"/>
   </objectgroup>
  </tile>
  <tile id="18">
   <objectgroup draworder="index">
-   <object id="0" x="-0.25" y="8.25" width="16.375" height="7.875"/>
+   <object id="0" x="0" y="8" width="16" height="8"/>
   </objectgroup>
  </tile>
  <tile id="19">
   <objectgroup draworder="index">
-   <object id="0" x="-0.125" y="8" width="16" height="8.375"/>
+   <object id="0" x="0" y="8" width="16" height="8"/>
   </objectgroup>
  </tile>
  <tile id="20">
   <objectgroup draworder="index">
-   <object id="0" x="7.875" y="8.125" width="8" height="8.125"/>
+   <object id="0" x="8" y="8" width="8" height="8"/>
   </objectgroup>
  </tile>
  <tile id="21">
   <objectgroup draworder="index">
-   <object id="0" x="-0.25" y="7.875" width="8" height="8.25"/>
+   <object id="0" x="0" y="8" width="8" height="8"/>
   </objectgroup>
  </tile>
  <tile id="24">
   <objectgroup draworder="index">
-   <object id="0" x="8" y="0.25" width="7.875" height="15.625"/>
+   <object id="0" x="8" y="0" width="8" height="16"/>
   </objectgroup>
  </tile>
  <tile id="25">
   <objectgroup draworder="index">
-   <object id="0" x="-0.125" y="0" width="7.875" height="16.25"/>
+   <object id="0" x="0" y="0" width="8" height="16"/>
   </objectgroup>
  </tile>
  <tile id="26">
   <objectgroup draworder="index">
-   <object id="0" x="0.125" y="0.25" width="15.75" height="7.5"/>
+   <object id="0" x="0" y="0" width="16" height="8"/>
   </objectgroup>
  </tile>
  <tile id="27">
@@ -116,12 +116,12 @@
  </tile>
  <tile id="28">
   <objectgroup draworder="index">
-   <object id="0" x="8" y="0.125" width="8.125" height="8"/>
+   <object id="0" x="8" y="0" width="8" height="8"/>
   </objectgroup>
  </tile>
  <tile id="29">
   <objectgroup draworder="index">
-   <object id="0" x="-0.125" y="-0.125" width="8" height="8.375"/>
+   <object id="0" x="0" y="0" width="8" height="8"/>
   </objectgroup>
  </tile>
 </tileset>
