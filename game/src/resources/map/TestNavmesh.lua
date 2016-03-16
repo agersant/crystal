dc9368a5e2c6a5018d35a83694b6c76cb77da4e7
Antoine Gersant <antoine.gersant@lesforges.org>
2016-03-15 23:33:12 -0700

    Added simple navmesh generation test

--- game/src/resources/map/TestNavmesh.lua
+++ game/src/resources/map/TestNavmesh.lua
@@ -1,4 +1,5 @@
 assert( gUnitTesting );
+local MapCollisionMesh = require( "src/resources/map/MapCollisionMesh" );
 
 local tests = {};
 
@@ -11,6 +12,13 @@ end
 tests[#tests + 1] = { name = "Load Navmesh Lua file" };
 tests[#tests].body = function()
 	local Navmesh = require( "src/resources/map/Navmesh" );
+end
+
+tests[#tests + 1] = { name = "Generate navmesh for empty map" };
+tests[#tests].body = function()
+	local Navmesh = require( "src/resources/map/Navmesh" );
+	local collisionMesh = MapCollisionMesh:new( 10, 10, 1 );
+	local navmesh = Navmesh:new( 10, 10, collisionMesh, 0 );
 end
 
 
