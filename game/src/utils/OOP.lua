6bf9d12a8a3557a0d7577e170326f1fa4e6fad2f
Antoine Gersant <antoine.gersant@lesforges.org>
2017-11-12 23:06:26 -0800

    Added sanity assert

--- game/src/utils/OOP.lua
+++ game/src/utils/OOP.lua
@@ -13,6 +13,7 @@ end
 
 local makeIsInstanceOf = function( class )
 	return function( self, otherClass )
+		assert( otherClass );
 		if class == otherClass then
 			return true;
 		end
@@ -28,31 +29,31 @@ local getClassName = function( self )
 end
 
 local declareClass = function( self, name, baseClass )
-	
+
 	local classMetaTable = {};
 	classMetaTable.__index = baseClass;
 	classMetaTable.__tostring = function( class )
 		return "Class definition of: " .. class._name;
 	end
 	local class = setmetatable( {}, classMetaTable );
-	
+
 	local objMetaTable = {};
 	objMetaTable.__index = class;
 	objMetaTable.__tostring = function( obj )
 		return "Instance of class: " .. obj._class._name;
 	end
-	
+
 	class._class = class;
 	class._name = name;
 	class._objMetaTable = objMetaTable;
-	
+
 	class.super = baseClass;
 	class.new = objectConstuctor;
 	class.getClassName = getClassName;
 	class.isInstanceOf = makeIsInstanceOf( class );
-	
+
 	classIndex[name] = class;
-	
+
 	return class;
 end
 
