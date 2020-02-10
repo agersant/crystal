ea49e2ac30aef21101324e8d229eb7ab884f3a97
Antoine Gersant <antoine.gersant@lesforges.org>
2020-02-10 00:17:01 -0800

    Added test coverage reports

--- game/main.lua
+++ game/main.lua
@@ -3,6 +3,7 @@ local Features = require("engine/dev/Features");
 MODULE = "arpg/ARPG";
 
 if Features.unitTesting then
+	require("external/luacov/runner").init({runreport = true, deletestats = true, exclude = {"^assets/.*$", "Test"}});
 	require("engine/dev/mock/love/graphics");
 	local TestSuite = require("engine/TestSuite");
 	local success = TestSuite.execute();
