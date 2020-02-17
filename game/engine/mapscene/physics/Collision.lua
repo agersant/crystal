c02afd60e198cfbf181b8b0709cfbba2227a5e8d
Antoine Gersant <antoine.gersant@lesforges.org>
2020-02-17 00:28:02 -0800

    Keep track of entities being touched

--- game/engine/mapscene/physics/Collision.lua
+++ game/engine/mapscene/physics/Collision.lua
@@ -1,5 +1,6 @@
 require("engine/utils/OOP");
 local Component = require("engine/ecs/Component");
+local TableUtils = require("engine/utils/TableUtils");
 
 local Collision = Class("Collision", Component);
 
@@ -7,6 +8,7 @@ Collision.init = function(self, radius)
 	Collision.super.init(self);
 	assert(radius);
 	self._shape = love.physics.newCircleShape(radius);
+	self._contactEntities = {};
 end
 
 Collision.getFixture = function(self)
@@ -21,10 +23,18 @@ Collision.getShape = function(self)
 	return self._shape;
 end
 
-Collision.onBeginTouch = function(self, otherEntity)
+Collision.onBeginTouch = function(self, otherComponent)
+	local entity = otherComponent:getEntity();
+	self._contactEntities[entity] = true;
 end
 
-Collision.onEndTouch = function(self, otherEntity)
+Collision.onEndTouch = function(self, otherComponent)
+	local entity = otherComponent:getEntity();
+	self._contactEntities[entity] = nil;
+end
+
+Collision.getContactEntities = function(self, otherComponent)
+	return TableUtils.shallowCopy(self._contactEntities);
 end
 
 return Collision;
