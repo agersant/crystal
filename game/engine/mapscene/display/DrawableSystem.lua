98688601e9559236cddd680e582729e6b9487bae
Antoine Gersant <antoine.gersant@lesforges.org>
2020-02-20 23:18:02 -0800

    Added shader component

--- game/engine/mapscene/display/DrawableSystem.lua
+++ game/engine/mapscene/display/DrawableSystem.lua
@@ -1,6 +1,7 @@
 require("engine/utils/OOP");
 local System = require("engine/ecs/System");
 local Drawable = require("engine/mapscene/display/Drawable");
+local Shader = require("engine/mapscene/display/Shader");
 
 local DrawableSystem = Class("DrawableSystem", System);
 
@@ -17,7 +18,15 @@ DrawableSystem.draw = function(self)
 	local drawables = ecs:getAllComponents(Drawable);
 	table.sort(drawables, sortDrawables);
 	for _, drawable in ipairs(drawables) do
+		local entity = drawable:getEntity();
+		local shader = entity:getComponent(Shader);
+		if shader then
+			shader:apply();
+		end
 		drawable:draw();
+		if shader then
+			love.graphics.setShader(nil);
+		end
 	end
 end
 
