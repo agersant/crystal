ad0b33c99cdb250bbda08507d20b8d84741ad261
Antoine Gersant <antoine.gersant@lesforges.org>
2020-02-23 16:50:01 -0800

    Move FFI file into its home

--- build_diamond_windows.ps1
+++ build_diamond_windows.ps1
@@ -3,3 +3,4 @@ Set-Location .\lib\diamond
 cargo build
 Pop-Location -StackName 'DirStack'
 Copy-Item .\lib\diamond\target\debug\diamond.dll .\game
+Copy-Item .\lib\diamond\ffi\Diamond.lua .\game\engine\ffi
