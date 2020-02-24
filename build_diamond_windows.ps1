8988a0a431c3564771f713fc29042bb65114b742
Antoine Gersant <antoine.gersant@lesforges.org>
2020-02-24 00:41:40 -0800

    Build diamond in release mode

--- build_diamond_windows.ps1
+++ build_diamond_windows.ps1
@@ -1,6 +1,6 @@
 Push-Location -StackName 'DirStack'
 Set-Location .\lib\diamond
-cargo build
+cargo build --release
 Pop-Location -StackName 'DirStack'
-Copy-Item .\lib\diamond\target\debug\diamond.dll .\game
+Copy-Item .\lib\diamond\target\release\diamond.dll .\game
 Copy-Item .\lib\diamond\ffi\Diamond.lua .\game\engine\ffi
