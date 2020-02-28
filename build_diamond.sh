93304411b01978d2a799b9d8bcce5777e0490605
Antoine Gersant <antoine.gersant@lesforges.org>
2020-02-27 23:19:56 -0800

    Create directory before putting files in it

--- build_diamond.sh
+++ build_diamond.sh
@@ -2,4 +2,5 @@ cd lib/diamond
 cargo build --release
 cd ../..
 cp ./lib/diamond/target/release/libdiamond.so ./game
+mkdir ./game/engine/ffi
 cp ./lib/diamond/target/diamond.lua ./game/engine/ffi/Diamond.lua
