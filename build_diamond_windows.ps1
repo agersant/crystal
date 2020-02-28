b8bba583fc8beb112276896dd3f618908d6ddcc4
Antoine Gersant <antoine.gersant@lesforges.org>
2020-02-27 23:42:23 -0800

    Create FFI directory if needed

--- build_diamond_windows.ps1
+++ build_diamond_windows.ps1
@@ -2,5 +2,6 @@ Push-Location -StackName 'DirStack'
 Set-Location .\lib\diamond
 cargo build --release
 Pop-Location -StackName 'DirStack'
+New-Item -Type dir .\game\engine\ffi
 Copy-Item .\lib\diamond\target\release\Diamond.dll .\game\diamond.dll
 Copy-Item .\lib\diamond\target\diamond.lua .\game\engine\ffi\Diamond.lua
