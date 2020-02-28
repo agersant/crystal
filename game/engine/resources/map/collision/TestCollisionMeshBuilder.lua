179431b1f952825340c02be0c804233c6a6083c2
Antoine Gersant <antoine.gersant@lesforges.org>
2020-02-28 00:41:19 -0800

    Fixed tests

--- game/engine/resources/map/collision/TestCollisionMeshBuilder.lua
+++ game/engine/resources/map/collision/TestCollisionMeshBuilder.lua
@@ -11,20 +11,20 @@ end
 
 tests[#tests + 1] = {name = "Single square"};
 tests[#tests].body = function()
-	local builder = CollisionMeshBuilder:new(50, 50);
+	local builder = CollisionMeshBuilder:new(50, 50, 10, 10);
 	builder:addPolygon(1, 1, {{x = 10, y = 10}, {x = 20, y = 10}, {x = 20, y = 20}, {x = 10, y = 20}});
 	local mesh = builder:buildMesh();
-	assert(#mesh:getChains() == 1);
+	assert(#mesh:getChains() == 2);
 end
 
 tests[#tests + 1] = {name = "Simple merge"};
 tests[#tests].body = function()
-	local builder = CollisionMeshBuilder:new(50, 50);
+	local builder = CollisionMeshBuilder:new(50, 50, 10, 10);
 	builder:addPolygon(1, 1, {{x = 10, y = 10}, {x = 20, y = 10}, {x = 20, y = 20}, {x = 10, y = 20}});
 	builder:addPolygon(2, 1, {{x = 20, y = 10}, {x = 30, y = 10}, {x = 30, y = 20}, {x = 20, y = 20}});
 	builder:addPolygon(0, 0, {{x = 0, y = 0}, {x = 5, y = 0}, {x = 5, y = 5}, {x = 0, y = 5}});
 	local mesh = builder:buildMesh();
-	assert(#mesh:getChains() == 2);
+	assert(#mesh:getChains() == 3);
 end
 
 return tests;
