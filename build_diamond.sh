5f06604a122fdcba83275d1d9417324fa7270ca8
Antoine Gersant <antoine.gersant@lesforges.org>
2020-02-27 23:20:08 -0800

    Cosmetic changes

--- build_diamond.sh
+++ build_diamond.sh
@@ -1,6 +1,7 @@
 cd lib/diamond
 cargo build --release
 cd ../..
-cp ./lib/diamond/target/release/libdiamond.so ./game
+
 mkdir ./game/engine/ffi
+cp ./lib/diamond/target/release/libdiamond.so ./game
 cp ./lib/diamond/target/diamond.lua ./game/engine/ffi/Diamond.lua
