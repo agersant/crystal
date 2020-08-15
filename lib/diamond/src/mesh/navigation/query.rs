853dc0e9a02fdd981cf5c29933211c958fa25959
Antoine Gersant <antoine.gersant@lesforges.org>
2020-08-14 17:00:17 -0700

    Cleanup

--- lib/diamond/src/mesh/navigation/query.rs
+++ lib/diamond/src/mesh/navigation/query.rs
@@ -1,23 +1,15 @@
-use crate::geometry::LineExt;
+use crate::geometry::*;
 use crate::mesh::navigation::*;
 use ordered_float::OrderedFloat;
 use pathfinding::prelude::*;
 
-// TODO implement as FaceHandle extension?
-fn face_center(face: &FaceHandle<Vertex, CdtEdge>) -> Point<f32> {
-	let triangle = face.as_triangle();
-	let x = (triangle[0][0] + triangle[1][0] + triangle[2][0]) / 3.0;
-	let y = (triangle[0][1] + triangle[1][1] + triangle[2][1]) / 3.0;
-	Point::new(x, y)
-}
-
 fn heuristic(from: &FaceHandle<Vertex, CdtEdge>, to: &Point<f32>) -> f32 {
-	let line = Line::new(face_center(from), to.clone());
+	let line = Line::new(from.center(), to.clone());
 	line.length()
 }
 
 fn movement_cost(from: &FaceHandle<Vertex, CdtEdge>, to: &FaceHandle<Vertex, CdtEdge>) -> f32 {
-	let line = Line::new(face_center(from), face_center(to));
+	let line = Line::new(from.center(), to.center());
 	line.length()
 }
 
@@ -33,7 +25,7 @@ fn funnel(
 	path.push(*from);
 	for face in triangle_path {
 		let face = mesh.triangulation.face(face);
-		path.push(face_center(&face));
+		path.push(face.center());
 	}
 	path.push(*to);
 	path
