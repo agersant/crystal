7c4a5379676982ddec669e09552cd0d168617e96
Antoine Gersant <antoine.gersant@lesforges.org>
2020-08-29 01:22:05 -0700

    Added test that sprites emit animation end event

--- game/engine/mapscene/display/TestSprite.lua
+++ game/engine/mapscene/display/TestSprite.lua
@@ -1,5 +1,9 @@
+local Entity = require("engine/ecs/Entity");
+local ScriptRunner = require("engine/mapscene/behavior/ScriptRunner");
 local Sprite = require("engine/mapscene/display/Sprite");
+local MapScene = require("engine/mapscene/MapScene");
 local Assets = require("engine/resources/Assets");
+local Script = require("engine/script/Script");
 
 local tests = {};
 
@@ -11,6 +15,32 @@ tests[#tests].body = function(context)
 	sprite:setAnimation("floating");
 	sprite:draw();
 	context:compareFrame("engine/test-data/TestSprite/sprites-can-draw.png");
+end
+
+tests[#tests + 1] = {name = "Emits animationEnd signal", gfx = "mock"};
+tests[#tests].body = function()
+	local scene = MapScene:new("engine/assets/empty_map.lua");
+	local entity = scene:spawn(Entity);
+
+	local sheet = Assets:getSpritesheet("engine/assets/blankey.lua");
+	local sprite = Sprite:new(sheet);
+	entity:addComponent(sprite);
+	sprite:setAnimation("floating");
+
+	local sentinel = false;
+	local scriptRunner = ScriptRunner:new();
+	entity:addComponent(scriptRunner);
+	scriptRunner:addScript(Script:new(function(self)
+		self:waitFor("animationEnd");
+		sentinel = true;
+	end));
+
+	scene:update(0.5);
+	assert(not sentinel);
+	scene:update(0.5);
+	assert(not sentinel);
+	scene:update(0.5);
+	assert(sentinel);
 end
 
 return tests;
