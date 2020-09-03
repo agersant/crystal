dcc0673711b43ac52ace22320a9a8761fde3401a
Antoine Gersant <antoine.gersant@lesforges.org>
2020-09-03 01:17:38 -0700

    Ported title screen to Bricks

--- game/arpg/frontend/TitleScreen.lua
+++ game/arpg/frontend/TitleScreen.lua
@@ -1,16 +1,31 @@
 require("engine/utils/OOP");
-local Widget = require("engine/ui/Widget");
-local Text = require("engine/ui/core/Text");
+local Fonts = require("engine/resources/Fonts");
+local HorizontalAlignment = require("engine/ui/bricks/core/HorizontalAlignment");
+local VerticalAlignment = require("engine/ui/bricks/core/VerticalAlignment");
+local Overlay = require("engine/ui/bricks/elements/Overlay");
+local Widget = require("engine/ui/bricks/elements/Widget");
+local Text = require("engine/ui/bricks/elements/Text");
+local UIScene = require("engine/ui/UIScene");
 
-local TitleScreen = Class("TitleScreen", Widget);
+local TitleScreenWidget = Class("TitleScreenWidget", Widget);
+local TitleScreen = Class("TitleScreen", UIScene);
 
--- PUBLIC API
+TitleScreenWidget.init = function(self)
+	TitleScreenWidget.super.init(self);
+
+	local overlay = Overlay:new();
+	self:setRoot(overlay);
+
+	local text = Text:new();
+	overlay:addChild(text);
+	text:setFont(Fonts:get("fat", 16));
+	text:setContent("Project Crystal");
+	text:setHorizontalAlignment(HorizontalAlignment.CENTER);
+	text:setVerticalAlignment(VerticalAlignment.CENTER);
+end
 
 TitleScreen.init = function(self)
-	TitleScreen.super.init(self);
-	local text = Text:new("fat", 16);
-	text:setText("Project Crystal");
-	self:addChild(text);
+	TitleScreen.super.init(self, TitleScreenWidget:new());
 end
 
 return TitleScreen;
--- game/engine/ui/UIScene.lua
+++ game/engine/ui/UIScene.lua
@@ -1,14 +1,21 @@
 require("engine/utils/OOP");
+local GFXConfig = require("engine/graphics/GFXConfig");
+local Widget = require("engine/ui/bricks/elements/Widget");
 local Scene = require("engine/Scene");
 
 local UIScene = Class("UIScene", Scene);
 
 UIScene.init = function(self, widget)
+	assert(widget);
+	assert(widget:isInstanceOf(Widget));
 	self._widget = widget;
 end
 
 UIScene.update = function(self, dt)
 	self._widget:update(dt);
+	local width, height = GFXConfig:getNativeSize();
+	self._widget:setLocalPosition(0, width, 0, height);
+	self._widget:layout();
 end
 
 UIScene.draw = function(self)
