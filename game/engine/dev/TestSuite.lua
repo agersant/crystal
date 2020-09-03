90306d0c6becb3890d6bd319beabb47c86e40e6c
Antoine Gersant <antoine.gersant@lesforges.org>
2020-09-03 01:12:32 -0700

    Added overlay tests to test suite

--- game/engine/dev/TestSuite.lua
+++ game/engine/dev/TestSuite.lua
@@ -28,6 +28,7 @@ local engineTestFiles = {
 	"engine/ui/bricks/core/TestContainer",
 	"engine/ui/bricks/core/TestWrapper",
 	"engine/ui/bricks/elements/TestHorizontalBox",
+	"engine/ui/bricks/elements/TestOverlay",
 	"engine/ui/bricks/elements/TestWidget",
 	"engine/ui/TestTextInput",
 	"engine/utils/TestAlias",
