28859a8d5c339486e9890d432ecc3c512b7820f9
Antoine Gersant <antoine.gersant@lesforges.org>
2020-09-07 16:11:07 -0700

    Don't enable collision components when physics body is missing

--- game/engine/mapscene/physics/CollisionSystem.lua
+++ game/engine/mapscene/physics/CollisionSystem.lua
@@ -2,12 +2,13 @@ require("engine/utils/OOP");
 local System = require("engine/ecs/System");
 local AllComponents = require("engine/ecs/query/AllComponents");
 local Collision = require("engine/mapscene/physics/Collision");
+local PhysicsBody = require("engine/mapscene/physics/PhysicsBody");
 
 local CollisionSystem = Class("CollisionSystem", System);
 
 CollisionSystem.init = function(self, ecs)
 	CollisionSystem.super.init(self, ecs);
-	self._query = AllComponents:new({Collision});
+	self._query = AllComponents:new({Collision, PhysicsBody});
 	self:getECS():addQuery(self._query);
 end
 
