9584e24a6aab72327e2fa86c6894e8207df54b8f
Antoine Gersant <antoine.gersant@lesforges.org>
2021-02-06 19:00:00 -0800

    Fixed capitalization

--- build_libs_linux.sh
+++ build_libs_linux.sh
@@ -8,4 +8,4 @@ cp ./lib/target/release/libdiamond.so ./game
 cp ./lib/target/release/diamond.lua ./game/engine/ffi/Diamond.lua
 
 cp ./lib/target/release/libknob.so ./game
-cp ./lib/target/release/knob.lua ./game/engine/ffi/knob.lua
+cp ./lib/target/release/knob.lua ./game/engine/ffi/Knob.lua
