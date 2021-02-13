3e93424a2b34f8e6aceca8160f4e50083a65665f
Antoine Gersant <antoine.gersant@lesforges.org>
2021-02-13 13:33:16 -0800

    Fixed broken colors

--- game/arpg/graphics/Palette.lua
+++ game/arpg/graphics/Palette.lua
@@ -6,4 +6,17 @@ Palette.black = {0, 0, 0};
 Palette.cyan = {0, 234, 255};
 Palette.strawberry = {231, 38, 38};
 
+-- TODO this is copy pasta from Engine.Colors
+
+local applyAlpha = function(color, alpha)
+	return {color[1], color[2], color[3], alpha};
+end
+
+for k, color in pairs(Palette) do
+	for i, c in ipairs(color) do
+		color[i] = c / 255;
+	end
+	color.alpha = applyAlpha;
+end
+
 return Palette;
