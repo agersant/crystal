630624ece549ae5b486382b16136eaa39991bacb
Antoine Gersant <antoine.gersant@lesforges.org>
2021-02-21 01:44:55 -0800

    Dont init engine before codecov startup

--- game/main.lua
+++ game/main.lua
@@ -3,17 +3,18 @@ local Features = require("engine/dev/Features");
 
 STARTUP_GAME = "arpg/ARPG"
 
-local engine = Engine:new(true);
-
 if Features.testing then
 	if Features.codeCoverage then
 		local luacov = require("external/luacov/runner");
 		local luacovExcludes = {"assets/.*$", "^main$", "Test", "test"};
 		luacov.init({runreport = true, exclude = luacovExcludes});
 	end
+	local engine = Engine:new(true);
 	local TestSuite = require("engine/dev/TestSuite");
 	local success = TestSuite:execute();
 	local exitCode = success and 0 or 1;
 	love.load = nil;
 	love.event.quit(exitCode);
+else
+	Engine:new(true);
 end
