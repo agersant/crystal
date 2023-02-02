45749bd156ae668252738efe80d45928a26edae6
Antoine Gersant <antoine.gersant@lesforges.org>
2023-02-02 02:54:36 -0800

    Tentative fix for flaky exit codes

--- game/main.lua
+++ game/main.lua
@@ -4,17 +4,25 @@ local Features = require("engine/dev/Features");
 STARTUP_GAME = "arpg/ARPG"
 
 if Features.testing then
+	local luacov;
 	if Features.codeCoverage then
-		local luacov = require("external/luacov/runner");
+		luacov = require("external/luacov/runner");
 		local luacovExcludes = { "assets/.*$", "^main$", "Test", "test" };
 		luacov.init({ runreport = true, exclude = luacovExcludes });
 	end
 	local engine = Engine:new(true);
 	local TestSuite = require("engine/dev/TestSuite");
 	local success = TestSuite:execute();
+	if luacov then
+		luacov.shutdown();
+	end
 	local exitCode = success and 0 or 1;
 	love.load = nil;
-	love.event.quit(exitCode);
+	love.run = function()
+		return function()
+			return exitCode;
+		end
+	end
 else
 	Engine:new(true);
 end
