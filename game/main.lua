99f812c43a66f3ad78e7ede64d51aca1e11f55fc
Antoine Gersant <antoine.gersant@lesforges.org>
2023-02-03 00:20:09 -0800

    Revert temp CI troubleshooting

--- game/main.lua
+++ game/main.lua
@@ -1,32 +1,29 @@
-print("Calling love.event.quit()");
-love.event.quit();
+local Engine = require("engine/Engine");
+local Features = require("engine/dev/Features");
 
--- local Engine = require("engine/Engine");
--- local Features = require("engine/dev/Features");
+STARTUP_GAME = "arpg/ARPG"
 
--- STARTUP_GAME = "arpg/ARPG"
-
--- if Features.testing then
--- 	local luacov;
--- 	if Features.codeCoverage then
--- 		luacov = require("external/luacov/runner");
--- 		local luacovExcludes = { "assets/.*$", "^main$", "Test", "test" };
--- 		luacov.init({ runreport = true, exclude = luacovExcludes });
--- 	end
--- 	local engine = Engine:new(true);
--- 	local TestSuite = require("engine/dev/TestSuite");
--- 	local success = TestSuite:execute();
--- 	if luacov then
--- 		print("Shutting down luacov");
--- 		luacov.shutdown();
--- 	end
--- 	local exitCode = success and 0 or 1;
--- 	love.run = function()
--- 		return function()
--- 			print("Exiting love.run with code " .. exitCode);
--- 			return exitCode;
--- 		end
--- 	end
--- else
--- 	Engine:new(true);
--- end
+if Features.testing then
+	local luacov;
+	if Features.codeCoverage then
+		luacov = require("external/luacov/runner");
+		local luacovExcludes = { "assets/.*$", "^main$", "Test", "test" };
+		luacov.init({ runreport = true, exclude = luacovExcludes });
+	end
+	local engine = Engine:new(true);
+	local TestSuite = require("engine/dev/TestSuite");
+	local success = TestSuite:execute();
+	if luacov then
+		print("Shutting down luacov");
+		luacov.shutdown();
+	end
+	local exitCode = success and 0 or 1;
+	love.run = function()
+		return function()
+			print("Exiting love.run with code " .. exitCode);
+			return exitCode;
+		end
+	end
+else
+	Engine:new(true);
+end
