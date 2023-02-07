e6fe75e7627f77b7f2e11d9c256357465d3e4108
Antoine Gersant <antoine.gersant@lesforges.org>
2023-02-06 21:45:32 -0800

    Test runner moved to init

--- runtime/init.lua
+++ runtime/init.lua
@@ -1,4 +1,5 @@
 require("utils/OOP");
+local Features = require("dev/Features");
 
 LOG = require("dev/Log"):new();
 TERMINAL = require("dev/cli/Terminal"):new();
@@ -119,4 +120,29 @@ end
 
 love.quit = function()
 	LIVE_TUNE:disconnectFromDevice();
+end
+
+if Features.tests then
+	local luacov;
+	if Features.codeCoverage then
+		luacov = require("external/luacov/runner");
+		local luacovExcludes = { "assets/.*$", "^main$", "Test", "test" };
+		luacov.init({ runreport = true, exclude = luacovExcludes });
+	end
+
+	local TestSuite = require("dev/TestSuite");
+	local success = TestSuite:execute();
+
+	if luacov then
+		luacov.shutdown();
+	end
+
+	love.quit();
+
+	local exitCode = success and 0 or 1;
+	love.run = function()
+		return function()
+			return exitCode;
+		end
+	end
 end
