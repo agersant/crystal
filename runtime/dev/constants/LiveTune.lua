319b953b9e966c293121fef7f37fa057f76bc9a4
Antoine Gersant <antoine.gersant@lesforges.org>
2023-02-09 02:03:58 -0800

    Re-enable Knob

--- runtime/dev/constants/LiveTune.lua
+++ runtime/dev/constants/LiveTune.lua
@@ -1,4 +1,4 @@
--- local Knob = require("knob"); -- TODO re-enable
+local Knob = require("knob");
 local Features = require("dev/Features");
 local MathUtils = require("utils/MathUtils");
 
@@ -85,41 +85,41 @@ TERMINAL:addCommand("connectToMIDIDevice port:number", function(port)
 	LIVE_TUNE:connectToDevice(port);
 end);
 
---#region Tests -- TODO re-enable
+--#region Tests
 
--- crystal.test.add("Lists devices", function()
--- 	local liveTune = LiveTune:new();
--- 	assert(type(liveTune:listDevices()) == "table")
--- end);
+crystal.test.add("Lists devices", function()
+	local liveTune = LiveTune:new();
+	assert(type(liveTune:listDevices()) == "table")
+end);
 
--- crystal.test.add("Can choose device", function()
--- 	local liveTune = LiveTune:new();
--- 	liveTune:connectToDevice(1);
--- end);
+crystal.test.add("Can choose device", function()
+	local liveTune = LiveTune:new();
+	liveTune:connectToDevice(1);
+end);
 
--- crystal.test.add("Can choose mode", function()
--- 	local liveTune = LiveTune:new();
--- 	liveTune:setMode("ABSOLUTE");
--- end);
+crystal.test.add("Can choose mode", function()
+	local liveTune = LiveTune:new();
+	liveTune:setMode("ABSOLUTE");
+end);
 
--- crystal.test.add("Unmapped knob reads as initial value", function()
--- 	local liveTune = LiveTune:new();
--- 	liveTune:mapKnobsToMIDI({});
--- 	local knobIndex = 1;
--- 	local initialValue = 5;
--- 	local value = liveTune:getValue(knobIndex, initialValue, 0, 10);
--- 	assert(value == initialValue);
--- end);
+crystal.test.add("Unmapped knob reads as initial value", function()
+	local liveTune = LiveTune:new();
+	liveTune:mapKnobsToMIDI({});
+	local knobIndex = 1;
+	local initialValue = 5;
+	local value = liveTune:getValue(knobIndex, initialValue, 0, 10);
+	assert(value == initialValue);
+end);
 
--- crystal.test.add("Retrieve current device", function()
--- 	local liveTune = LiveTune:new();
--- 	local device = liveTune:getCurrentDevice();
--- 	assert(type(device) == "nil" or type(device) == "string");
--- end);
+crystal.test.add("Retrieve current device", function()
+	local liveTune = LiveTune:new();
+	local device = liveTune:getCurrentDevice();
+	assert(type(device) == "nil" or type(device) == "string");
+end);
 
--- crystal.test.add("Has global API", function()
--- 	assert(LIVE_TUNE);
--- end);
+crystal.test.add("Has global API", function()
+	assert(LIVE_TUNE);
+end);
 
 --#endregion
 
