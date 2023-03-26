dc69c5f73d8a0309d3b513bb083f2bd9d3c4e34c
Antoine Gersant <antoine.gersant@lesforges.org>
2023-03-25 21:04:54 -0700

    Color constructor

--- runtime/modules/graphics/color.lua
+++ runtime/modules/graphics/color.lua
@@ -13,20 +13,15 @@ local hex_to_rgb = function(hex)
 	return r, g, b;
 end
 
-Color.init = function(self)
-	self[1], self[2], self[3], self[4] = 0, 0, 0, 1;
-end
-
----@param hex number
----@param alpha? number
----@return Color
-Color.hex = function(self, hex, alpha)
-	local self = Color:new();
-	assert(type(hex) == "number");
+Color.init = function(self, rgb, alpha)
+	assert(rgb == nil or type(rgb) == "number");
 	assert(alpha == nil or type(alpha) == "number");
-	self[1], self[2], self[3] = hex_to_rgb(hex);
+	if rgb then
+		self[1], self[2], self[3] = hex_to_rgb(rgb);
+	else
+		self[1], self[2], self[3] = 0, 0, 0;
+	end
 	self[4] = alpha or 1;
-	return self;
 end
 
 ---@return Color
@@ -55,43 +50,43 @@ end
 
 -- Debug draw colors
 -- https://flatuicolors.com/palette/nl
-Color.sunflower = Color:hex(0xFFC312);
-Color.radiant_yellow = Color:hex(0xF79F1F);
-Color.puffins_bill = Color:hex(0xEE5A24);
-Color.red_pigment = Color:hex(0xEA2027);
-Color.energos = Color:hex(0xC4E538);
-Color.android_green = Color:hex(0xA3CB38);
-Color.pixelated_grass = Color:hex(0x009432);
-Color.turkish_aqua = Color:hex(0x006266);
-Color.blue_martina = Color:hex(0x12CBC4);
-Color.mediterranean_sea = Color:hex(0x1289A7);
-Color.merchant_marine_blue = Color:hex(0x0652DD);
-Color.leagues_under_the_sea = Color:hex(0x1B1464);
-Color.lavender_rose = Color:hex(0xFDA7DF);
-Color.lavender_tea = Color:hex(0xD980FA);
-Color.forgotten_purple = Color:hex(0x9980FA);
-Color.circumorbital_ring = Color:hex(0x5758BB);
-Color.bara_red = Color:hex(0xED4C67);
-Color.very_berry = Color:hex(0xB53471);
-Color.hollyhock = Color:hex(0x833471);
-Color.magenta_purple = Color:hex(0x6F1E51);
+Color.sunflower = Color:new(0xFFC312);
+Color.radiant_yellow = Color:new(0xF79F1F);
+Color.puffins_bill = Color:new(0xEE5A24);
+Color.red_pigment = Color:new(0xEA2027);
+Color.energos = Color:new(0xC4E538);
+Color.android_green = Color:new(0xA3CB38);
+Color.pixelated_grass = Color:new(0x009432);
+Color.turkish_aqua = Color:new(0x006266);
+Color.blue_martina = Color:new(0x12CBC4);
+Color.mediterranean_sea = Color:new(0x1289A7);
+Color.merchant_marine_blue = Color:new(0x0652DD);
+Color.leagues_under_the_sea = Color:new(0x1B1464);
+Color.lavender_rose = Color:new(0xFDA7DF);
+Color.lavender_tea = Color:new(0xD980FA);
+Color.forgotten_purple = Color:new(0x9980FA);
+Color.circumorbital_ring = Color:new(0x5758BB);
+Color.bara_red = Color:new(0xED4C67);
+Color.very_berry = Color:new(0xB53471);
+Color.hollyhock = Color:new(0x833471);
+Color.magenta_purple = Color:new(0x6F1E51);
 
 -- Tool colors
-Color.black = Color:hex(0x000000);
-Color.white = Color:hex(0xFFFFFF);
-Color.red = Color:hex(0xFF5733);
-Color.green = Color:hex(0x84D728);
-Color.cyan = Color:hex(0x00B3CC);
-Color.grey0 = Color:hex(0x11121D);
-Color.greyA = Color:hex(0x1A1B2B);
-Color.greyB = Color:hex(0x22263D);
-Color.greyC = Color:hex(0x373C53);
-Color.greyD = Color:hex(0xB0BED5);
+Color.black = Color:new(0x000000);
+Color.white = Color:new(0xFFFFFF);
+Color.red = Color:new(0xFF5733);
+Color.green = Color:new(0x84D728);
+Color.cyan = Color:new(0x00B3CC);
+Color.grey0 = Color:new(0x11121D);
+Color.greyA = Color:new(0x1A1B2B);
+Color.greyB = Color:new(0x22263D);
+Color.greyC = Color:new(0x373C53);
+Color.greyD = Color:new(0xB0BED5);
 
 --#region Tests
 
 crystal.test.add("Can create color from hex value", function()
-	local color = Color:hex(0x686de0, 0.6);
+	local color = Color:new(0x686de0, 0.6);
 	local r, g, b, a = color:components();
 	assert(r == 104 / 255);
 	assert(g == 109 / 255);
@@ -100,7 +95,7 @@ crystal.test.add("Can create color from hex value", function()
 end);
 
 crystal.test.add("Can alter color alpha", function()
-	local color = Color:hex(0x686de0, 0.6);
+	local color = Color:new(0x686de0, 0.6);
 	local opaque = color:alpha(1);
 	local r, g, b, a = opaque:components();
 	assert(r == 104 / 255);
