1b89df0a82b063703d8f9e07ebb793a5c84580f3
Antoine Gersant <antoine.gersant@lesforges.org>
2023-04-03 20:58:06 -0700

    AI debug draw paths

--- runtime/modules/ai/ai_system.lua
+++ runtime/modules/ai/ai_system.lua
@@ -18,28 +18,67 @@ crystal.cmd.add("showNavigationOverlay", function() draw_navigation = true; end)
 crystal.cmd.add("hideNavigationOverlay", function() draw_navigation = false; end);
 
 AISystem.draw_debug = function(self, viewport)
+	if draw_navigation then
+		self:draw_navigation_mesh(viewport);
+		self:draw_paths(viewport);
+	end
+end
+
+---@private
+AISystem.draw_navigation_mesh = function(self, viewport)
 	local line_color = crystal.Color.lavender_rose;
 	local fill_color = line_color:alpha(.25);
-	if draw_navigation then
-		love.graphics.push("all");
-		love.graphics.setLineWidth(1);
-		love.graphics.setLineJoin("bevel");
-		love.graphics.setPointSize(4 * viewport:getZoom());
 
-		local map = self:ecs():context("map");
-		local triangles = {};
-		for _, t in ipairs(map:navigation_polygons()) do
-			local vertices = { t[1][1], t[1][2], t[2][1], t[2][2], t[3][1], t[3][2] };
-			love.graphics.setColor(fill_color);
-			love.graphics.polygon("fill", vertices);
+	love.graphics.push("all");
+	love.graphics.setLineWidth(1);
+	love.graphics.setLineJoin("bevel");
+	love.graphics.setPointSize(4 * viewport:getZoom());
 
-			love.graphics.setColor(line_color);
-			love.graphics.polygon("line", vertices);
-			love.graphics.points(vertices);
-		end
+	local map = self:ecs():context("map");
+	assert(map);
+	local triangles = {};
+	for _, t in ipairs(map:navigation_polygons()) do
+		local vertices = { t[1][1], t[1][2], t[2][1], t[2][2], t[3][1], t[3][2] };
+		love.graphics.setColor(fill_color);
+		love.graphics.polygon("fill", vertices);
 
-		love.graphics.pop();
+		love.graphics.setColor(line_color);
+		love.graphics.polygon("line", vertices);
+		love.graphics.points(vertices);
 	end
+
+	love.graphics.pop();
+end
+
+---@private
+AISystem.draw_paths = function(self, viewport)
+	love.graphics.push("all");
+	love.graphics.setLineWidth(2);
+	love.graphics.setLineJoin("bevel");
+	love.graphics.setPointSize(6 * viewport:getZoom());
+	love.graphics.setColor(crystal.Color.bara_red);
+	for navigation in pairs(self.query:components()) do
+		local path, index = navigation:navigation_state();
+		if path then
+			-- Current segment
+			local x, y = navigation:entity():position();
+			love.graphics.line(x, y, path[index][1], path[index][2]);
+
+			-- Future segments
+			local points = {};
+			for i = index, #path do
+				table.push(points, path[i][1]);
+				table.push(points, path[i][2]);
+			end
+			if #points >= 4 then
+				love.graphics.line(points);
+			end
+			if #points >= 2 then
+				love.graphics.points(points);
+			end
+		end
+	end
+	love.graphics.pop();
 end
 
 --#region Tests
--- runtime/modules/ai/navigation.lua
+++ runtime/modules/ai/navigation.lua
@@ -4,16 +4,27 @@ local PositionGoal = require("modules/ai/position_goal");
 
 ---@class Navigation : Component
 ---@field private script Script
+---@field private path {[1]: number, [2]: number}[]
+---@field private path_index number
 local Navigation = Class("Navigation", crystal.Component);
 
 local navigate;
 
 Navigation.init = function(self)
 	self.script = crystal.Script:new();
+	self.path = nil;
+	self.path_index = nil;
 end
 
+---@dt number
 Navigation.update = function(self, dt)
 	self.script:update(dt);
+end
+
+---@return {[1]: number, [2]: number}[] path
+---@return number path_index
+Navigation.navigation_state = function(self)
+	return self.path, self.path_index;
 end
 
 ---@param x number
@@ -58,6 +69,7 @@ end
 Navigation.navigate_to_goal = function(self, goal, repath_delay)
 	assert(goal);
 
+	local navigation = self;
 	local body = self:entity():component(crystal.Body);
 	local movement = self:entity():component(crystal.Movement);
 	local map = self:entity():context("map");
@@ -87,7 +99,7 @@ Navigation.navigate_to_goal = function(self, goal, repath_delay)
 			while true do
 				self:thread(function(self)
 					self:stop_on("repath");
-					if navigate(self, map, goal, body, movement) then
+					if navigation:navigate(self, map, goal, body, movement) then
 						self:signal("success");
 					else
 						self:signal("failure");
@@ -101,45 +113,46 @@ Navigation.navigate_to_goal = function(self, goal, repath_delay)
 	end);
 end
 
----@param self Thread
+---@private
+---@param thread Thread
 ---@param goal Goal
 ---@param body Body
 ---@param movement Movement
 ---@return boolean
-navigate = function(self, map, goal, body, movement)
+Navigation.navigate = function(self, thread, map, goal, body, movement)
 	if not goal:is_valid() then
 		return false;
 	end
 
 	local x, y = body:position();
 	local target_x, target_y = goal:position();
-	local path = map:find_path(x, y, target_x, target_y);
-	if not path then
+	self.path = map:find_path(x, y, target_x, target_y);
+	if not self.path then
 		return false;
 	end
 
-	local index = 1;
+	self.path_index = 1;
 	while true do
 		if goal:is_valid() and goal:is_position_acceptable(body:position()) then
 			return true;
 		end
 
-		local waypoint = path[index];
+		local waypoint = self.path[self.path_index];
 		if not waypoint then
 			break;
 		end
 		local waypoint_x, waypoint_y = waypoint[1], waypoint[2];
 		local x, y = body:position();
 		local distance_squared = math.distance_squared(x, y, waypoint_x, waypoint_y);
-		local epsilon = movement:speed() * self:delta_time();
+		local epsilon = movement:speed() * thread:delta_time();
 		if distance_squared >= epsilon * epsilon then
 			local dx, dy = waypoint_x - x, waypoint_y - y;
 			local rotation = math.atan2(dy, dx);
 			movement:set_heading(rotation);
-			self:wait_frame();
+			thread:wait_frame();
 		else
 			body:set_position(waypoint_x, waypoint_y);
-			index = index + 1;
+			self.path_index = self.path_index + 1;
 		end
 	end
 
